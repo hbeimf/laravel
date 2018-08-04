84fceca0411b54ed4e75fa510ff6b7a6990a1f00
maomao <you@example.com>
2018-08-04 15:07:33 +0800

    register

--- manager.demo.com/app/Http/Controllers/API/UserController.php
+++ manager.demo.com/app/Http/Controllers/API/UserController.php
@@ -1,17 +1,7 @@
 <?php
 // https://www.jianshu.com/p/2988ba405b3b?from=timeline&isappinstalled=0
 
-// CREATE TABLE `users` (
-//   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-//   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-//   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-//   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-//   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-//   `created_at` timestamp NULL DEFAULT NULL,
-//   `updated_at` timestamp NULL DEFAULT NULL,
-//   PRIMARY KEY (`id`),
-//   UNIQUE KEY `users_email_unique` (`email`)
-// ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci 
+// http://la.demo.com/api/register/?name=test1&email=123456@qq.com&password=123456&c_password=123456
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
@@ -55,6 +45,7 @@ class UserController extends Controller
      */
     public function register(Request $request)
     {
+        // var_dump($request->all());exit;
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'email' => 'required|email',
