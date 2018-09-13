6fe6b0880cd86097269cde4ebb03363c350af9e6
maomao <5622876@qq.com>
2018-09-13 15:25:09 +0800

    enablelog

--- manager.demo.com/app/Providers/AppServiceProvider.php
+++ manager.demo.com/app/Providers/AppServiceProvider.php
@@ -2,29 +2,30 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;   //新增的
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-        Schema::defaultStringLength(191);  //新增的
-    }
+//新增的
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		//
+		DB::connection()->enableQueryLog();
+		Schema::defaultStringLength(191); //新增的
+	}
+
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		//
+	}
 }
